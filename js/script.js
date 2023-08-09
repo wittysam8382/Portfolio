0b8640dfdca43538c3a20de3ce802b79a00c2e64
wittysam8382 <101207674+wittysam8382@users.noreply.github.com>
2023-08-09 18:48:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+let sections= documnet.querySelectorAll('section');
+let navlinks= document.querySelectorAll('header nav a');
+
+window.onscroll=()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset=sec.offsetTop-150;
+        let height= sec.offsetHeight;
+        let id= sec.getAttribut('id');
+
+        if(top>=offset && top<offset + height){
+            navlinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+            })
+        }
+
+    });
+    let head= document.querySelector('header');
+    Headers.classList.toggle('sticky',window.scrollY>100);
+};
